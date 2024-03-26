deb77f578ad26d8e0631e05d97488bb543ceac78
shakibmojumderinfo <shakibmojumder99@gmail.com>
2024-03-26 11:52:58 +0600

    modified

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 
+function setCountdown() {
+    minutes = document.getElementById("minutesInput").value;
+    seconds = document.getElementById("secondsInput").value;
+
+     countDownDate = new Date().getTime() + (minutes * 60000) + (seconds * 1000);
+
+    const x = setInterval( function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "Countdown End";
+            
+    }
+
+         min = Math.round((distance % (1000 * 60 * 60)) / (1000 * 60));
+         sec = Math.round((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = min + " m " + sec + " s";
+    }, 1000);
+}
+
