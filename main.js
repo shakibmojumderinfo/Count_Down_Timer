3be60ff654cac1e6483603025a859ef98b51a0f9
shakibmojumderinfo <shakibmojumder99@gmail.com>
2024-08-31 00:42:05 +0600

    modification

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-function setCountdown() {
-    minutes = document.getElementById("minutesInput").value;
-    seconds = document.getElementById("secondsInput").value;
+// function setCountdown() {
+//     minutes = document.getElementById("minutesInput").value;
+//     seconds = document.getElementById("secondsInput").value;
 
-     countDownDate = new Date().getTime() + (minutes * 60000) + (seconds * 1000);
+//      countDownDate = new Date().getTime() + (minutes * 60000) + (seconds * 1000);
 
-    const x = setInterval( function() {
-        var now = new Date().getTime();
-        var reverseTIme = countDownDate - now;
+//     const x = setInterval( function() {
+//         var now = new Date().getTime();
+//         var reverseTIme = countDownDate - now;
 
-        if (reverseTIme < 0) {
-            clearInterval(x);
-            document.getElementById("countdown").innerHTML = "Countdown End";
+//         if (reverseTIme < 0) {
+//             clearInterval(x);
+//             document.getElementById("countdown").innerHTML = "Countdown End";
             
-    }
+//     }
 
-         min = Math.round((reverseTIme % (1000 * 60 * 60)) / (1000 * 60));
-         sec = Math.round((reverseTIme % (1000 * 60)) / 1000);
+//          min = Math.round((reverseTIme % (1000 * 60 * 60)) / (1000 * 60));
+//          sec = Math.round((reverseTIme % (1000 * 60)) / 1000);
 
-        document.getElementById("countdown").innerHTML = min + " m " + sec + " s";
-    }, 1000);
-}
+//         document.getElementById("countdown").innerHTML = min + " m " + sec + " s";
+//     }, 1000);
+// }
 
